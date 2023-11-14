8338a56c4597bed46a90098c4ebe6c1367de4c18
Createmynick <max2001-17@mail.ru>
2023-11-14 06:58:38 +0200

    2 revew

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,28 @@
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы:
 const placesList = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки:
-const createCards = function (createCard, deleteCard) {
+function createCards(cardData, deleteCard) {
   const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardItem.querySelector('.card__image');
   const cardTitle = cardItem.querySelector('.card__title');
   const deleteButton = cardItem.querySelector('.card__delete-button');
-  cardTitle.textContent = createCard.name;
-  cardImage.src = createCard.link;
-  // @todo: Функция удаления карточки:
-  deleteButton.addEventListener('click', function () {
-    cardItem.remove();
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardItem);
   });
+
   return cardItem;
-};
+}
+// @todo: Функция удаления карточки:
+function deleteCard(cardItem) {
+  cardItem.remove();
+}
 // @todo: Вывести карточки на страницу:
 initialCards.forEach(function (card) {
-  const cardDone = createCards(card);
+  const cardDone = createCards(card, deleteCard);
   placesList.append(cardDone);
 });
